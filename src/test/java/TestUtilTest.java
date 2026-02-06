68658f1b2fb138fe26316f808c266a77a6e9a0e8
minho <minho01@hs.ac.kr>
2026-02-06 10:17:48 +0900

    입력 자동화

---
+++ src/test/java/TestUtilTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class TestUtilTest {
+    @Test
+    void t1(){
+
+        final String out = TestUtil.run("""
+                등록
+                현재를 사랑하라
+                작자미상
+                """);
+
+        assert
+
+    }
+}
